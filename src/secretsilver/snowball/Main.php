e0ac05d4de10396367a3920fefe14a4339ea122b
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-06-11 16:40:04 -0400

    Update Main.php

--- src/secretsilver/snowball/Main.php
+++ src/secretsilver/snowball/Main.php
@@ -4,6 +4,7 @@ namespace secretsilver\snowball;
 use pocketmine\event\Listener;
 use pocketmine\level\Explosion;
 use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\entity\Living;
 use pocketmine\plugin\PluginBase;
   
 class Main extends PluginBase implements Listener {
