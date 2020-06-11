6026c523cbf8c4b819dd7b5c301bd80332963061
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-06-11 16:51:25 -0400

    Update Main.php

--- src/secretsilver/snowball/Main.php
+++ src/secretsilver/snowball/Main.php
@@ -10,8 +10,8 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase implements Listener {
 
   public function snowballEntityHit(ProjectileHitEntityEvent $e){
-    if($e->getEntity() instanceof Living){
-        $explosion = new Explosion($e->getEntity(), 5, null);
+    if($e->getEntityHit() instanceof Living){
+        $explosion = new Explosion($e->getEntityHit(), 5, null);
         $explosion->explodeB();
     }
   }
