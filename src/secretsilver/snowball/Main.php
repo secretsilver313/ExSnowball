8fef0c8c49f9dedfa79c8dadf6d35adb61f3049b
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-06-11 16:23:03 -0400

    Update Main.php

--- src/secretsilver/snowball/Main.php
+++ src/secretsilver/snowball/Main.php
@@ -3,7 +3,7 @@
 namespace secretsilver\snowball;
 
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\Listeners;
+use pocketmine\event\Listener;
 use pocketmine\level\Explosion;
 use pocketmine\event\entity\SnowballEntityHitEvent;
 use pocketmine\plugin\PluginBase;
@@ -12,7 +12,6 @@ class Main extends PluginBase implements Listener {
 
   public function snowballEntityHit(ProjectileHitEntityEvent $e){
     if($e->getEntity() instanceof Living){
-        //The first parameter is the position of the explosion, entities extend Position, so we can directly pass the entity class, 2nd parameter is the size, change it to whatever works ok, and the third is
         $explosion = new Explosion($e->getEntity(), 5, null);
         $explosion->explodeB();
         
